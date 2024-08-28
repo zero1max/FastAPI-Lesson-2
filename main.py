90c21488c0b9f7f780b61d151b0df56cbf9d5d08
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-08-28 15:14:12 +0500

    Second Commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from typing import Annotated
 
 app = FastAPI()
@@ -29,8 +29,48 @@ app = FastAPI()
 
 # ----------------------------------------------------------------------------------------------------------
 
-feed_query = Query(max_length=30)
+# feed_query = Query(max_length=30)
 
-@app.get('/news/feed')
-async def news_feed(search: Annotated[list[str] | None, feed_query] = None):
-    return {"news": ['new1', 'new2', 'new3'], 'serach_key':search}
+# @app.get('/news/feed')
+# async def news_feed(search: Annotated[list[str] | None, feed_query] = None):
+#     return {"news": ['new1', 'new2', 'new3'], 'serach_key':search}
+
+# ----------------------------------------------------------------------------------------------------------
+
+# feed_query = Query(max_length=30, title='Qidiruv kaliti', description="Lorem Ipsum")
+
+# @app.get('/news/feed')
+# async def news_feed(search: Annotated[str , feed_query] = 'all'):
+#     return {"news": ['new1', 'new2', 'new3'], 'serach_key':search}
+
+# ----------------------------------------------------------------------------------------------------------
+
+# feed_query = Query(max_length=30, title='Qidiruv kaliti', description="Lorem Ipsum", alias='news-query', include_in_schema=False)
+
+# @app.get('/news/feed')
+# async def news_feed(search: Annotated[str , feed_query] = 'all'):
+#     if search:
+#         pass
+#     return {"news": ['new1', 'new2', 'new3'], 'serach_key':search}
+
+# ----------------------------------------------------------------------------------------------------------
+
+# @app.get('/news/feed/{category}')
+# async def news_feed(
+#     category: str, 
+#     search: str = 'all'):
+#     if search:
+#         pass
+#     return {"news": ['new1', 'new2', 'new3'],"category": category ,'serach_key':search}
+
+# ----------------------------------------------------------------------------------------------------------
+
+@app.get('/news/feed/{new_id}')
+async def news_feed(
+    new_id: Annotated[int, Path(title='News ID', gt=0, le=100)], 
+    search: str = 'all'):
+    if search:
+        pass
+    return {"news": ['new1', 'new2', 'new3'], "new_id": new_id ,'serach_key':search}
+
+# gt = greater than > >= gt. ge    =====    < <=  lt. le
